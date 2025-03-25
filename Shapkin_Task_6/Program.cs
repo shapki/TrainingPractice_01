5edbab3b621904897af55a4abe37005aee97acd1
Игорь Шапкин <shapkin.igorek@gmail.com>
2025-03-25 13:10:26 +0300

    reworking task 5

--- Shapkin_Task_6/Program.cs
+++ Shapkin_Task_6/Program.cs
@@ -16,7 +16,7 @@ namespace Shapkin_Task_6
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Task6());
         }
     }
 }
---
+++ Shapkin_Task_6/Task6.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Shapkin_Task_6
+{
+    public partial class Task6 : Form
+    {
+        public Task6()
+        {
+            InitializeComponent();
+        }
+    }
+}
