fea095e193eb57713f6a75f00f485ae0cde35d4a
Игорь Шапкин <shapkin.igorek@gmail.com>
2025-03-25 13:55:31 +0300

    Fixed task1 code

--- TrainingPractice_01/Program.cs
+++ TrainingPractice_01/Program.cs
@@ -7,15 +7,25 @@
         Console.Write($"-- Добро пожаловать в магазин Кристалов по {CrystalPrice} золота!\n");
 
         Console.Write("| Сколько золота у вас? : ");
-        int initialGoldAmount = int.Parse(Console.ReadLine());
+        int initialGoldAmount = ReadPositiveInt();
 
         Console.Write("| Сколько кристалов вы хотите купить? : ");
-        int crystalsToBuy = int.Parse(Console.ReadLine());
+        int crystalsToBuy = ReadPositiveInt();
 
         int totalPurchaseCost = CalculatePurchaseCost(crystalsToBuy);
         (int remainingGold, int acquiredCrystals, string purchaseMessage) = PerformPurchase(initialGoldAmount, totalPurchaseCost, crystalsToBuy);
 
         DisplayPurchaseResult(remainingGold, acquiredCrystals, purchaseMessage);
+    }
+
+    static int ReadPositiveInt()
+    {
+        int value;
+        while (!int.TryParse(Console.ReadLine(), out value) || value < 0)
+        {
+            Console.Write("| Введите только положительное число! : ");
+        }
+        return value;
     }
 
     static int CalculatePurchaseCost(int crystalsToBuy)
@@ -25,14 +35,16 @@
 
     static (int, int, string) PerformPurchase(int goldAmount, int purchaseCost, int crystalsToBuy)
     {
-        bool isPurchaseSuccessful = goldAmount >= purchaseCost;
-        int remainingGold = isPurchaseSuccessful ? goldAmount - purchaseCost : goldAmount;
-        int acquiredCrystals = isPurchaseSuccessful ? crystalsToBuy : 0;
+        int remainingGold = goldAmount - purchaseCost;
+        int acquiredCrystals = crystalsToBuy;
+
+        remainingGold = Math.Max(remainingGold, goldAmount);
+        acquiredCrystals = Math.Min(acquiredCrystals, (goldAmount >= purchaseCost ? crystalsToBuy : 0));
 
         string successMessage = $"| Теперь у вас {remainingGold} золота и {acquiredCrystals} кристаллов.";
         string failureMessage = $"Покупка не удалась. У вас осталось {goldAmount} золота";
 
-        string message = isPurchaseSuccessful ? successMessage : failureMessage;
+        string message = (goldAmount >= purchaseCost ? successMessage : failureMessage);
 
         return (remainingGold, acquiredCrystals, message);
     }
@@ -42,3 +54,4 @@
         Console.WriteLine(purchaseMessage);
     }
 }
+
