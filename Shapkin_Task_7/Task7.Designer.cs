64fae1d2e6ad1776e9b9459d244e09bca1ea72fe
Игорь Шапкин <shapkin.igorek@gmail.com>
2025-03-26 14:28:54 +0300

    Started coding task 7

---
+++ Shapkin_Task_7/Task7.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Shapkin_Task_7
+{
+    partial class Task7
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.foxCountLabel = new System.Windows.Forms.Label();
+            this.chickenCountLabel = new System.Windows.Forms.Label();
+            this.moveLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 7;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 351F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(598, 247);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // foxCountLabel
+            // 
+            this.foxCountLabel.Location = new System.Drawing.Point(0, 250);
+            this.foxCountLabel.Name = "foxCountLabel";
+            this.foxCountLabel.Size = new System.Drawing.Size(100, 23);
+            this.foxCountLabel.TabIndex = 2;
+            this.foxCountLabel.Text = "Лис: ?";
+            this.foxCountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // chickenCountLabel
+            // 
+            this.chickenCountLabel.Location = new System.Drawing.Point(187, 250);
+            this.chickenCountLabel.Name = "chickenCountLabel";
+            this.chickenCountLabel.Size = new System.Drawing.Size(100, 23);
+            this.chickenCountLabel.TabIndex = 3;
+            this.chickenCountLabel.Text = "Кур: ?";
+            this.chickenCountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // moveLabel
+            // 
+            this.moveLabel.BackColor = System.Drawing.Color.DimGray;
+            this.moveLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.moveLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.moveLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.moveLabel.Location = new System.Drawing.Point(0, 276);
+            this.moveLabel.Name = "moveLabel";
+            this.moveLabel.Size = new System.Drawing.Size(287, 23);
+            this.moveLabel.TabIndex = 4;
+            this.moveLabel.Text = "Ходят: ?";
+            this.moveLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Task7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 299);
+            this.Controls.Add(this.moveLabel);
+            this.Controls.Add(this.chickenCountLabel);
+            this.Controls.Add(this.foxCountLabel);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Task7";
+            this.Text = "Лисы и куры";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label foxCountLabel;
+        private System.Windows.Forms.Label chickenCountLabel;
+        private System.Windows.Forms.Label moveLabel;
+    }
+}
+
---
+++ Shapkin_Task_7/Task7.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Shapkin_Task_7
+{
+    public partial class Task7 : Form
+    {
+        public Task7()
+        {
+            InitializeComponent();
+            this.HelpButtonClicked += ShapedForm1_HelpButtonClicked;
+        }
+
+        private void ShapedForm1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            if (MessageBox.Show("TODO: description", "Лисы и куры - Информация", MessageBoxButtons.OK, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
