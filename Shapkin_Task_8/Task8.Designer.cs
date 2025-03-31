3bc0cc448b3858cf09065afc2364a3994500e6ae
Игорь Шапкин <shapkin.igorek@gmail.com>
2025-03-31 14:31:12 +0300

    Coded task 8

---
+++ Shapkin_Task_8/Task8.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Shapkin_Task_8
+{
+    partial class Task8
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gridSizeLabel = new System.Windows.Forms.Label();
+            this.gridSizeTextBox = new System.Windows.Forms.TextBox();
+            this.timeIntervalLabel = new System.Windows.Forms.Label();
+            this.timeIntervalTextBox = new System.Windows.Forms.TextBox();
+            this.reinfectionProbabilityLabel = new System.Windows.Forms.Label();
+            this.reinfectionProbabilityTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.gridPanel = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.stopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gridSizeLabel
+            // 
+            this.gridSizeLabel.AutoSize = true;
+            this.gridSizeLabel.Location = new System.Drawing.Point(12, 9);
+            this.gridSizeLabel.Name = "gridSizeLabel";
+            this.gridSizeLabel.Size = new System.Drawing.Size(98, 13);
+            this.gridSizeLabel.TabIndex = 0;
+            this.gridSizeLabel.Text = "Размер сетки (N):";
+            // 
+            // gridSizeTextBox
+            // 
+            this.gridSizeTextBox.Location = new System.Drawing.Point(107, 6);
+            this.gridSizeTextBox.MaxLength = 3;
+            this.gridSizeTextBox.Name = "gridSizeTextBox";
+            this.gridSizeTextBox.Size = new System.Drawing.Size(56, 20);
+            this.gridSizeTextBox.TabIndex = 1;
+            this.gridSizeTextBox.Text = "21";
+            this.gridSizeTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.gridSizeTextBox_KeyPress);
+            // 
+            // timeIntervalLabel
+            // 
+            this.timeIntervalLabel.AutoSize = true;
+            this.timeIntervalLabel.Location = new System.Drawing.Point(169, 9);
+            this.timeIntervalLabel.Name = "timeIntervalLabel";
+            this.timeIntervalLabel.Size = new System.Drawing.Size(82, 13);
+            this.timeIntervalLabel.TabIndex = 2;
+            this.timeIntervalLabel.Text = "Интервал (мс):";
+            // 
+            // timeIntervalTextBox
+            // 
+            this.timeIntervalTextBox.Location = new System.Drawing.Point(248, 6);
+            this.timeIntervalTextBox.MaxLength = 5;
+            this.timeIntervalTextBox.Name = "timeIntervalTextBox";
+            this.timeIntervalTextBox.Size = new System.Drawing.Size(56, 20);
+            this.timeIntervalTextBox.TabIndex = 3;
+            this.timeIntervalTextBox.Text = "500";
+            this.timeIntervalTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.timeIntervalTextBox_KeyPress);
+            // 
+            // reinfectionProbabilityLabel
+            // 
+            this.reinfectionProbabilityLabel.AutoSize = true;
+            this.reinfectionProbabilityLabel.Location = new System.Drawing.Point(310, 9);
+            this.reinfectionProbabilityLabel.Name = "reinfectionProbabilityLabel";
+            this.reinfectionProbabilityLabel.Size = new System.Drawing.Size(126, 13);
+            this.reinfectionProbabilityLabel.TabIndex = 4;
+            this.reinfectionProbabilityLabel.Text = "Вер повтор заражения:";
+            // 
+            // reinfectionProbabilityTextBox
+            // 
+            this.reinfectionProbabilityTextBox.Location = new System.Drawing.Point(432, 6);
+            this.reinfectionProbabilityTextBox.MaxLength = 4;
+            this.reinfectionProbabilityTextBox.Name = "reinfectionProbabilityTextBox";
+            this.reinfectionProbabilityTextBox.Size = new System.Drawing.Size(40, 20);
+            this.reinfectionProbabilityTextBox.TabIndex = 5;
+            this.reinfectionProbabilityTextBox.Text = "0.1";
+            this.reinfectionProbabilityTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.reinfectionProbabilityTextBox_KeyPress);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.SeaGreen;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.startButton.Location = new System.Drawing.Point(10, 32);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(231, 37);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Старт";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // gridPanel
+            // 
+            this.gridPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.gridPanel.Location = new System.Drawing.Point(0, 75);
+            this.gridPanel.Name = "gridPanel";
+            this.gridPanel.Size = new System.Drawing.Size(483, 484);
+            this.gridPanel.TabIndex = 7;
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.Color.Maroon;
+            this.stopButton.Enabled = false;
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.stopButton.Location = new System.Drawing.Point(241, 32);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(231, 37);
+            this.stopButton.TabIndex = 8;
+            this.stopButton.Text = "Стоп";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Task8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 559);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.gridPanel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.reinfectionProbabilityTextBox);
+            this.Controls.Add(this.reinfectionProbabilityLabel);
+            this.Controls.Add(this.timeIntervalTextBox);
+            this.Controls.Add(this.timeIntervalLabel);
+            this.Controls.Add(this.gridSizeTextBox);
+            this.Controls.Add(this.gridSizeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Task8";
+            this.Text = "Инфекция гйда";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label gridSizeLabel;
+        private System.Windows.Forms.TextBox gridSizeTextBox;
+        private System.Windows.Forms.Label timeIntervalLabel;
+        private System.Windows.Forms.TextBox timeIntervalTextBox;
+        private System.Windows.Forms.Label reinfectionProbabilityLabel;
+        private System.Windows.Forms.TextBox reinfectionProbabilityTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Panel gridPanel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
---
+++ Shapkin_Task_8/Task8.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Shapkin_Task_8
+{
+    public partial class Task8 : Form
+    {
+        private int _gridSize;
+        private int _timeInterval;
+        private double _reinfectionProbability;
+        private CellState[,] _grid;
+        private const int ImmuneDuration = 4;
+        private const int InfectionDuration = 6;
+        private const double InfectionProbability = 0.5;
+        public Color HealthyColor = Color.White;
+        public Color InfectedColor = Color.Red;
+        public Color ImmuneColor = Color.Green;
+
+        private SimulationFacade _simulationFacade;
+        private GridRenderer _gridRenderer;
+
+        public Task8()
+        {
+            InitializeComponent();
+            timer1.Tick += TimerTick;
+            _simulationFacade = new SimulationFacade();
+            _gridRenderer = new GridRenderer(gridPanel);
+
+            HelpButtonClicked += ShapedForm1_HelpButtonClicked;
+        }
+
+        private void ShapedForm1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            if (MessageBox.Show("Симуляция инфекций.\n\nБелые клетки - инфекции нет\nКрасные клетки - инфекция есть\nЗеленые клетки - болеющие", "Инфекция гйда - Информация", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                e.Cancel = true;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _gridSize = int.Parse(gridSizeTextBox.Text);
+                if (_gridSize % 2 == 0)
+                {
+                    MessageBox.Show("Размер сетки должен быть нечетным!");
+                    return;
+                }
+
+                _timeInterval = int.Parse(timeIntervalTextBox.Text);
+                _reinfectionProbability = double.Parse(reinfectionProbabilityTextBox.Text, CultureInfo.InvariantCulture);
+
+                _simulationFacade.InitializeGrid(_gridSize);
+                _grid = _simulationFacade.GetGrid();
+
+                StopInfection(false);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка ввода: {ex.Message}");
+            }
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            _simulationFacade.SimulateInfection(_reinfectionProbability, InfectionDuration, ImmuneDuration, InfectionProbability);
+            _grid = _simulationFacade.GetGrid();
+            UpdateGridVisual();
+        }
+
+        private void UpdateGridVisual()
+        {
+            _gridRenderer.UpdateGridVisual(_grid, _gridSize, HealthyColor, InfectedColor, ImmuneColor);
+        }
+
+        private void TextBoxKeyPress_DigitsOnly(KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void TextBoxKeyPress_BinaryOnly(KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && e.KeyChar != '0' && e.KeyChar != '1' && e.KeyChar != '.')
+                e.Handled = true;
+        }
+
+        private void gridSizeTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBoxKeyPress_DigitsOnly(e);
+        }
+
+        private void timeIntervalTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBoxKeyPress_DigitsOnly(e);
+        }
+
+        private void reinfectionProbabilityTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBoxKeyPress_BinaryOnly(e);
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            StopInfection(true);
+        }
+
+        private void StopInfection(bool val)
+        {
+            gridSizeTextBox.Enabled = val;
+            timeIntervalTextBox.Enabled = val;
+            reinfectionProbabilityTextBox.Enabled = val;
+            startButton.Enabled = val;
+            stopButton.Enabled = !val;
+
+            if (val)
+                timer1.Stop();
+            else
+            {
+                timer1.Interval = _timeInterval;
+                timer1.Start();
+            }
+        }
+    }
+
+    public enum State
+    {
+        Healthy,
+        Infected,
+        Immune
+    }
+}
